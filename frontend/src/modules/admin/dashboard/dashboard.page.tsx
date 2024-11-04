8c5f4d55c7cc2b8bbff17c325dca3c8a20ea6872
narra <ermanfaminiano020@gmail.com>
2024-11-05 02:41:54 +0800

    feat: :lipstick: added header ui

--- frontend/src/modules/admin/dashboard/dashboard.page.tsx
+++ frontend/src/modules/admin/dashboard/dashboard.page.tsx
@@ -4,19 +4,31 @@ import Announcement from '../../../components/announcements/announcement';
 import pic from '../../../assets/image/jpg/error.jpg';
 const DashboardPage: FC = () => {
   return (
-    <div>
-      <div className="flex justify-between p-2">
-        <Announcement
-          title="ANNOUNCEMENT TITLE"
-          description="DESCRIPTION"
-          imageUrl={pic}
-          date="02-20-2002"
-        ></Announcement>
-        <div className="ml-auto">
-          <CustomMonthLayout />
+    <>
+      <div className="bg-blue-900 p-4 text-white">
+        <h1 className="text-xl">Good Day!</h1>
+      </div>
+      <div>
+        <div className="flex justify-between p-2">
+          <Announcement
+            title="ANNOUNCEMENT TITLE"
+            description="DESCRIPTION"
+            imageUrl={pic}
+            date="02-20-2002"
+          ></Announcement>
+          <Announcement
+            title="ANNOUNCEMENT TITLE"
+            description="description here"
+            imageUrl={pic}
+            date="02-20-2002"
+          ></Announcement>
+
+          <div className="ml-auto">
+            <CustomMonthLayout />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
