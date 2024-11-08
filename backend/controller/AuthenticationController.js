0c290de533608824f5092740a288363776edecdf
Bals <balsamojj04@gmail.com>
2024-11-09 01:49:52 +0800

    login progress

--- backend/controller/AuthenticationController.js
+++ backend/controller/AuthenticationController.js
@@ -53,7 +53,7 @@ module.exports = {
                 res.json({
                     message: `Logged in! User ID: ${user.user_id} Username: ${user.username} User type: ${user.role_id}`,
                     accessToken: accessToken,
-                    userType: user.user_type
+                    role_id: user.role_id
                 });
             }
 
