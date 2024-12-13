83f19943d3b8c52843ec009fdfcd3b1f690a5ce8
narra <ermanfaminiano020@gmail.com>
2024-12-13 08:05:22 +0800

    feat: :sparkles: update

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'votingDB',
-    dialect: 'mysql',
-    pool:{
-        min: 0,
-        max: 5,
-        acquire: 30000,
-        idle: 10000
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
+    pool: {
+        min: parseInt(process.env.DB_POOL_MIN, 10),
+        max: parseInt(process.env.DB_POOL_MAX, 10),
+        acquire: parseInt(process.env.DB_POOL_ACQUIRE, 10),
+        idle: parseInt(process.env.DB_POOL_IDLE, 10),
     }
-}
+};
