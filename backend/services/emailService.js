e64bd8d0f8e8cf56fcaf562a4e5a9ca1b6e0955e
narra <ermanfaminiano020@gmail.com>
2024-12-17 12:39:39 +0800

    feat: :sparkles: change of email and pass

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -2,12 +2,12 @@ const nodemailer = require('nodemailer');
 
 // Create a transporter
 const transporter = nodemailer.createTransport({
-    host: "smtp.hostinger.com", // Use your SMTP server host (e.g., Gmail, Outlook, etc.)
-    port: 465,              // Use 465 for secure, or 587 for non-secure
-    secure: true,          // Use true for 465, false for others
+    host: "smtp.hostinger.com", 
+    port: 465,              
+    secure: true,         
     auth: {
-        user: "flexperienceadmin@flexperience.pro", // Your email address
-        pass: "Capstone-123"     // Your email password or app password
+        user: "vnhssslg2024@gmail.com", 
+        pass: "SSLG@2024"    
     }
 });
 
@@ -15,11 +15,11 @@ const transporter = nodemailer.createTransport({
 const sendEmail = async (to, subject, text, html) => {
     try {
         const info = await transporter.sendMail({
-            from: 'flexperienceadmin@flexperience.pro', // Sender's email
-            to,                                           // Recipient's email
-            subject,                                      // Subject line
-            text,                                         // Plain text version
-            html                                          // HTML version
+            from: 'vnhssslg2024@gmail.com', 
+            to,                                           
+            subject,                                    
+            text,                                        
+            html                                          
         });
         console.log(`Email sent to ${to}`);
         return info;
