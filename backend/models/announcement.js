5db71e7f1fca0e7c83fd37aceef8f86b7e062f75
narra <ermanfaminiano020@gmail.com>
2024-12-30 14:54:44 +0800

    feat: :zap: fix thy code

--- backend/models/announcement.js
+++ backend/models/announcement.js
@@ -1,35 +1,44 @@
 module.exports = (sequelize, Sequelize) => {
     const Announcement = sequelize.define('Announcement', {
+  
         announcement_id: {
             type: Sequelize.INTEGER,
             primaryKey: true,
             autoIncrement: true,
             allowNull: false,
         },
+   
         title_header: {
             type: Sequelize.STRING,
             allowNull: false,
         },
+     
         time_date: {
             type: Sequelize.DATE,
             allowNull: false,
         },
+    
         image_url: {
             type: Sequelize.TEXT,
             allowNull: true,
         },
+      
         description_text: {
             type: Sequelize.TEXT,
             allowNull: false,
         },
-        status: { // New column
+      
+        status: {
             type: Sequelize.STRING,
             allowNull: false,
             defaultValue: 'active',
+            validate: {
+                isIn: [['active', 'inactive']], 
+            },
         },
     }, {
-        tableName: 'announcements',
-        timestamps: false,
+        tableName: 'announcements', 
+        timestamps: true, 
     });
 
     return Announcement;
